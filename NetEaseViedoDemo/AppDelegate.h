6e981fd0a8e56dca3a98ec150cec3a9deb91f451
Deft_MKJing <mkjing@mkjingdemac-mini.local>
2016-08-31 09:56:33 +0800

    Initial Commit

---
+++ NetEaseViedoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetEaseViedoDemo
+//
+//  Created by MKJING on 16/8/31.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetEaseViedoDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NetEaseViedoDemo
+//
+//  Created by MKJING on 16/8/31.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
